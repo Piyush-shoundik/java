3b345b34c6541a5ef5dfb8f86c2da39763715160
Piyush-shoundik <ritetopiyush@gmail.com>
2026-02-10 11:15:46 +0530

    firsta_app

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,13 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("   ***   " + " " + "   ***   " + " " + " ******" + " " + "  **** ");
-        System.out.println(" **   ** " + " " + " **   ** " + " " + " **  **" + " " + " **    ");
-        System.out.println("**     **" + " " + "**     **" + " " + " **  **" + " " + "**     ");
-        System.out.println("**     **" + " " + "**     **" + " " + " ******" + " " + " ***** ");
-        System.out.println(" **   ** " + " " + " **   ** " + " " + " **    " + " " + "     **");
-        System.out.println("  ** **  " + " " + "  ** **  " + " " + " **    " + " " + "**   **");
-        System.out.println("   ***   " + " " + "   ***   " + " " + " **    " + " " + "  ***  "); 
+        // System.out.println("   ***   " + " " + "   ***   " + " " + " ******" + " " + "  **** ");
+        // System.out.println(" **   ** " + " " + " **   ** " + " " + " **  **" + " " + " **    ");
+        // System.out.println("**     **" + " " + "**     **" + " " + " **  **" + " " + "**     ");
+        // System.out.println("**     **" + " " + "**     **" + " " + " ******" + " " + " ***** ");
+        // System.out.println(" **   ** " + " " + " **   ** " + " " + " **    " + " " + "     **");
+        // System.out.println("  ** **  " + " " + "  ** **  " + " " + " **    " + " " + "**   **");
+        // System.out.println("   ***   " + " " + "   ***   " + " " + " **    " + " " + "  ***  "); 
+
+        System.out.println("THIS IS MY FIRST APP");
     }
 }
