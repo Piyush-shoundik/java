807a85e6238710ed07133fe5e746b6f57ccf7790
Piyush-shoundik <ritetopiyush@gmail.com>
2026-02-17 10:30:48 +0530

    Development: Create a simple plus and minus calculator

--- Basic-java/sum.java
+++ Basic-java/sum.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
 public class sum {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Your first number here");
+        int a = sc.nextInt();
+
+        System.out.println("Enter your second number here");
+        int b = sc.nextInt();
+
+        int c = a + b;
+        System.out.println("Total sum is: " + c );
+
+    }
 }
